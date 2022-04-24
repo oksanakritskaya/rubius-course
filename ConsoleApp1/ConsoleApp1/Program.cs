61fb0a5b8bfb2be0416255a6832bb4ea4a290539
oksana.kritskaya <oksana.kritskaya@rubius.com>
2022-04-24 20:44:23 +0700

    Added Lesson2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Lesson2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello world");
+            Console.Read();
+        }
+    }
+}
