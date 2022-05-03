ab0c2b17d9cfad1f194341f6234fa9163ba2c592
oksana.kritskaya <oksana.kritskaya@rubius.com>
2022-05-03 17:54:53 +0700

    Added Lesson3

---
+++ ConsoleApp1/Lesson3/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Lesson3
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("a: ");
+            int a = NormalizeNum(Console.ReadLine());
+
+            Console.Write("action: ");
+            string action = Console.ReadLine();
+
+            Console.Write("b: ");
+            int b = NormalizeNum(Console.ReadLine());
+
+            Console.WriteLine("____________________");
+
+            int result = Calculate(a, b, action);
+
+            Console.WriteLine($"{result}");
+        }
+
+        public static int NormalizeNum(string num = "")
+        {
+            int result = 0;
+
+            bool success = Int32.TryParse(num, out result);
+
+            return result;
+        }
+
+        public static int Calculate(int a, int b, string action = "")
+        {
+            int result = 0;
+
+            switch (action)
+            {
+                case "+": 
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "%":
+                    result = a % b;
+                    break;
+                default:
+                    Console.WriteLine("Неизвестный оператор");
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
