a00388266606147c4d33ee829bd5922aed9171dd
oksana.kritskaya <oksana.kritskaya@rubius.com>
2022-05-12 20:16:45 +0700

    Added Lesson5

---
+++ ConsoleApp1/Lesson5/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lesson5;
+
+public static class Program
+{
+    public static void Main()
+    {
+        Vector vector = new Vector(1, 2, 3);
+        Console.WriteLine(vector.getVectorLength());
+
+        Vector vector2 = new Vector(4, 5, 6);
+        Console.WriteLine(vector.getScalarMultiplication(vector2));
+
+        Console.WriteLine(vector.getVectorMultiplication(vector2).getValues());
+
+        Console.WriteLine(vector.getAngle(vector2));
+
+        Console.WriteLine(vector.getSum(vector2).getValues());
+        Console.WriteLine(vector.getDiff(vector2).getValues());
+    }
+}
---
+++ ConsoleApp1/Lesson5/Vector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    internal class Vector
+    {
+        private double x;
+        private double y;
+        private double z;
+
+        public Vector(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public string getValues()
+        {
+            return $"({x}, {y}, {z})";
+        }
+
+        public double getVectorLength()
+        {
+            return Math.Sqrt(getSecondPow(this.x) + getSecondPow(this.x) + getSecondPow(this.x));
+        }
+
+        public double getScalarMultiplication(Vector b)
+        {
+            return x * b.x + y * b.y + z * b.z;
+        }
+
+        public Vector getVectorMultiplication(Vector b)
+        {
+            return new Vector(y * b.z - z * b.y, z * b.x - x * b.z, x * b.y - y * b.x);
+        }
+
+        public double getAngle(Vector b)
+        {
+            return getScalarMultiplication(b) / getVectorLength() * b.getVectorLength();
+        }
+
+        public Vector getSum(Vector b)
+        {
+            return new Vector(x + b.x, y + b.y, z + b.z);
+        }
+
+        public Vector getDiff(Vector b)
+        {
+            return new Vector(x - b.x, y - b.y, z - b.z);
+        }
+
+        private double getSecondPow(double value)
+        {
+            return Math.Pow(value, 2);
+        }
+    }
+}
